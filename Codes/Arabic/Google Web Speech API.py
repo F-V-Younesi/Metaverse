69a06d716cb68cc8e4c5f55225bb78ea676adc71
Vahid.Unesi <90507840+F-V-Younesi@users.noreply.github.com>
2025-04-06 00:31:20 -0700

    Add files via upload

---
+++ Codes/Arabic/Google Web Speech API.py
@@ -0,0 +1,24 @@
+# ----Google Web Speech API-----
+#Best in time
+
+#Package Instalation:
+! pip install SpeechRecognition
+
+import speech_recognition as sr
+import time
+sr.__version__
+
+# Initialize the recognizer
+recognizer = sr.Recognizer()
+s=time.time()
+r = sr.Recognizer()
+sound = sr.AudioFile('/content/test1.wav')
+with sound as source:
+    audio = r.record(source)
+    try:
+        # using google speech recognition
+        print("Text: "+r.recognize_google(audio, language='ar-AR'))
+    except:
+         print("Sorry, I did not get that")#print(r.recognize_google(audio))
+e=time.time()
+print(e-s)
